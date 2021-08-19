baead09698923bdbb6dc20b55efbde51f4dd54c7
triplethriled <86870412+triplethriled@users.noreply.github.com>
2021-08-19 23:04:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const http = require('http');
 
 const express = require('express');
 const cors = require('cors');
-// const port = process.env.PORT || 80;
-const port = 8000
+const port = process.env.PORT || 8000;
+//const port = 8000
 const app = express();
 
 app.use(express.json());
